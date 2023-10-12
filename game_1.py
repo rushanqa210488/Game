46e391e2f9ca7906bfda56953f229f3a17d7c614
Rushan <rushan.akhmetov@inbox.ru>
2023-10-13 02:22:42 +0300

    Modified file game_1.py

--- game_1.py
+++ game_1.py
@@ -1,6 +1,5 @@
 import random
 
-num = random.randint(1, 100)
 print("Добро пожаловать в числовую угадайку")
 
 def is_valid(numb):
@@ -13,19 +12,32 @@ def is_valid(numb):
     else:
         return False
 
-while True:
-    num_user = input("Введите число от 1 до 100: ")
-    if not is_valid(num_user):
-        print("А может быть все-таки введем целое число от 1 до 100?")
-        continue
-    if not 1 <= int(num_user) <= 100:
-        print("А может быть все-таки введем целое число от 1 до 100?")
-    else:
-        if int(num_user) < num:
-            print("Ваше число меньше загаданного, попробуйте еще разок")
-        elif int(num_user) > num:
-            print("Ваше число больше загаданного, попробуйте еще разок")
+def number_guessing_game():
+    numb_bord = int(input())
+    num = random.randint(1, numb_bord)
+    counter = 0
+    while True:
+        counter += 1
+        num_user = input(f"Введите число от 1 до {numb_bord}: ")
+        if not is_valid(num_user):
+            print(f"А может быть все-таки введем целое число от 1 до {numb_bord}?")
+            continue
+        if not 1 <= int(num_user) <= numb_bord:
+            print(f"А может быть все-таки введем целое число от 1 до {numb_bord}?")
         else:
-            print("Вы угадали, поздравляем!")
-            print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
-            break
+            if int(num_user) < num:
+                print("Ваше число меньше загаданного, попробуйте еще разок")
+            elif int(num_user) > num:
+                print("Ваше число больше загаданного, попробуйте еще разок")
+            else:
+                print("Вы угадали, поздравляем!" f"C {counter} попыток")
+                break
+
+number_guessing_game()
+
+one_more_time = input("Сыграем еще раз? Введите 'ДА' или 'НЕТ': ").upper()
+if one_more_time == 'ДА':
+    print("OK")
+    number_guessing_game()
+else:
+    print("Спасибо, что играли в числовую угадайку. Еще увидимся...")
